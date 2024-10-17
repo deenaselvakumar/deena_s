9904e472c8e903fffe712f4fd2bbb5565b6a11e5
deenaselvakumar <deenavpuram@gmail.com>
2024-10-17 11:27:58 +0530

    one

--- 1.py
+++ 1.py
@@ -1,5 +1,9 @@
-a=12
-b=11
-c=a+b
-print(c)
-print("heloo guys")
+def remove_smallest(arr):
+    if arr:
+        arr.remove(min(arr)) 
+    return arr
+
+# Example usage:
+arr = [5,3,4,5,2,4,3,2,1,4,1]
+result = remove_smallest(arr)
+print(result) 
